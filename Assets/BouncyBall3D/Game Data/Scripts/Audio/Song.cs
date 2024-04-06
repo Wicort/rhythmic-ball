d83d949f3952145b96c6b93f56b539e900cb19bc
Wicort <wicort@yandex.ru>
2024-04-06 16:33:41 +0300

    initial

---
+++ Assets/BouncyBall3D/Game Data/Scripts/Audio/Song.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Song", menuName = "Create Song Asset")]
+public class Song : ScriptableObject
+{
+    public AudioClip song;
+    public int BPM;
+    public int stars;
+    public int difficulty = 1;
+
+    public float TimeFromBeat(int beat)
+    {
+        return song.length / (song.length / 60 * BPM) * beat;
+    }
+
+    public int BeatFromTime(float time)
+    {
+        return (int)((time * song.length) / 60 * BPM);
+    }
+
+    public void LoadData()
+    {
+        stars = PlayerPrefs.GetInt(name + "Stars", 0);
+    }
+
+    public void SaveData()
+    {
+        PlayerPrefs.SetInt(name + "Stars", stars);
+    }
+}
---
+++ Assets/BouncyBall3D/Game Data/Scripts/UI/SongHolder.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SongHolder : MonoBehaviour
+{
+    [SerializeField] Song song;
+    [SerializeField] Text songName;
+    [SerializeField] Image[] stars = new Image[3];
+    [SerializeField] Image[] sculls = new Image[5];
+    [SerializeField] Color activeStars, inactiveStars;
+    [SerializeField] Color activeSculls, inactiveSculls;
+
+
+    public void SetSong(Song newSong)
+    {
+        song = newSong;
+        UpdateInfo();
+    }
+
+    public void UpdateInfo()
+    {
+        song.LoadData();
+
+        for (int i = 0; i < 3; i++)
+            stars[i].color = i < song.stars ? activeStars : inactiveStars;
+
+        for (int i = 0; i < 5; i++)
+            sculls[i].color = i < song.difficulty ? activeSculls : inactiveSculls;
+
+        songName.text = song.name;
+    }
+
+    public void PlaySong()
+    {
+        LevelGenerator.Instance.currentSong = song;
+        LevelGenerator.Instance.StartWithSong();
+        UIManager.Instance.CloseMenu();
+    }
+}
